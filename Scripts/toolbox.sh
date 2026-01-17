9d5e051f8bc73c90a2b04a2651f78e6ae61ba028
Alex Diker <23444429+alexdcodes@users.noreply.github.com>
2026-01-17 03:15:28 -0500

    Toolbox.sh
    creates a menu so you can remove and enter containers (still adding more like creating a container etc)

---
+++ Scripts/toolbox.sh
@@ -0,0 +1,49 @@
+#
+# Simple Instructions: 
+#
+#
+# place the shell script in any folder you will no delete
+# note down the folder/toolbox.sh 
+# chmod +X toolbox.sh than 
+# add ||  alias te="~/path/toolbox.sh" || to .bashrc 
+#
+
+ 
+clear
+echo "**************************************************"
+cat << "EOF"
+                             A   D                                                                           
+▗▄▄▄▖▄▄▄   ▄▄▄  █ ▗▖    ▄▄▄  ▄   ▄     ▗▖  ▗▖▗▞▀▚▖▄▄▄▄  █  ▐▌
+  █ █   █ █   █ █ ▐▌   █   █  ▀▄▀      ▐▛▚▞▜▌▐▛▀▀▘█   █ ▀▄▄▞▘
+  █ ▀▄▄▄▀ ▀▄▄▄▀ █ ▐▛▀▚▖▀▄▄▄▀ ▄▀ ▀▄     ▐▌  ▐▌▝▚▄▄▖█   █      
+  █             █ ▐▙▄▞▘                ▐▌  ▐▌                
+                        Podman/Toolbox                                                      
+        https://github.com/alexdcodes/Fedora-Kinoite-Changes                                                   
+	v.1 (More commands/options coming for v2)     
+                                                               
+EOF
+echo "***************************************************"
+toolbox list
+echo ""
+echo ""
+echo -n "ALERT! Do you want to 'enter' or 'stop' a container (Enter/Stop)?: "
+read reply
+echo ""
+
+if [[ "${reply,,}" =~ ^(enter|e)$ ]]; then
+    echo "INFO: you selected enter container..."
+    echo ""
+    echo -n "Enter container name: "
+    read cname
+    toolbox enter "$cname"
+else
+    toolbox list
+    echo "----------------------------------------------"
+    read -p "Enter the toolbox(container) you want to Stop? " container_name
+    echo "----------------------------------------------"
+    podman stop "$container_name" || echo "Failed to stop (not running or missing), continuing..."
+    echo ""
+    echo "INFO Listing all stopped containers on the system: "
+    toolbox list  # Fixed typo: listt -> list
+fi
+
